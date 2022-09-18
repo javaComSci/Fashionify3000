4e71a6e52f21fcb69d2657eb2562de5df95f18fc
javaComSci <iramanat@purdue.edu>
2022-09-18 12:53:01 -0700

    resizing

--- Model/data_loader.py
+++ Model/data_loader.py
@@ -8,6 +8,54 @@ TEST = "test"
 VALIDATION = "validation"
 
 class DataLoader:
+    def resize_images(self, type):
+        basePath = "clothing-dataset-small"
+        basePathWithType = basePath + "/" + type
+
+        newPath = "clothing-dataset-resized"
+        newPathWithType = newPath + "/" + type
+        if os.path.exists(newPathWithType):
+            shutil.rmtree(newPathWithType)
+        os.makedirs(dir)
+
+        rowsCounts = {}
+        colsCounts = {}
+    
+        for encodedClothingType in os.listdir(os.fsencode(basePathWithType)):
+            decodedClothingType = os.fsdecode(encodedClothingType)
+            clothingTypePath = basePathWithType + "/" + decodedClothingType
+
+            if decodedClothingType == ".DS_Store":
+                continue
+
+            for encodedClothing in os.listdir(os.fsencode(clothingTypePath)):
+                decodedClothing = os.fsdecode(encodedClothing)
+                clothingPath = clothingTypePath + "/" + decodedClothing
+                clothingImage = Image.open(clothingPath)
+                clothing = np.asarray(clothingImage)
+
+                if clothing.shape[0] not in rowsCounts:
+                    rowsCounts[clothing.shape[0]] = 1
+                else:
+                    rowsCounts[clothing.shape[0]] += 1
+                
+                if clothing.shape[1] not in colsCounts:
+                    colsCounts[clothing.shape[1]] = 1
+                else:
+                    colsCounts[clothing.shape[1]] += 1
+                
+                width, height = clothingImage.size
+                print(clothingPath)
+                print(clothing.shape)
+                print("IMAGE", height, width)
+
+        # Majority of rows are 711 or 400, so will resize them to 400
+        print("Rows lengths counts", rowsCounts)
+
+        # Majority of cols are 400
+        print("Cols lengths counts", colsCounts)
+
+
     def load_data(self, type):
         data = []
 
@@ -17,10 +65,13 @@ class DataLoader:
         for encodedClothingType in os.listdir(os.fsencode(basePathWithType)):
             decodedClothingType = os.fsdecode(encodedClothingType)
             clothingTypePath = basePathWithType + "/" + decodedClothingType
+
+            if decodedClothingType == ".DS_Store":
+                continue
+
             for encodedClothing in os.listdir(os.fsencode(clothingTypePath)):
                 decodedClothing = os.fsdecode(encodedClothing)
                 clothingPath = clothingTypePath + "/" + decodedClothing
-                print(clothingPath)
                 clothingImage = Image.open(clothingPath)
                 clothing = np.asarray(clothingImage)
                 data.append(clothing)
@@ -29,4 +80,4 @@ class DataLoader:
 
 
 loader = DataLoader()
-loader.load_data(TEST)
+loader.resize_images(TEST)
